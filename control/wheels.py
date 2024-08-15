67211976866105118174de65e5e6b52529bd7dff
retinfai <aferetipama@gmail.com>
2024-08-15 18:13:25 +1200

    Add wheels class and servo factory

---
+++ control/wheels.py
@@ -0,0 +1,34 @@
+from util import set_velocity, servo_factory
+
+
+class Wheels:
+    def __init__(self):
+
+        self.servos = []
+
+        for _ in range(2):
+            self.servos.append(
+                servo_factory.create_servo(
+                    model="MX-106",
+                    port="/dev/ttyUSB0",
+                    protocol=1,
+                    baudrate=1000000,
+                    max=4095,
+                    min=0,
+                )
+            )
+
+    def move_forward(self, speed):
+        set_velocity(self.servos, [speed, speed])
+
+    def move_backward(self, speed):
+        set_velocity(self.servos, [-speed, -speed])
+
+    def turn_clockwise(self, speed):
+        set_velocity(self.servos, [-speed, speed])
+
+    def turn_counter_clockwise(self, speed):
+        set_velocity(self.servos, [speed, -speed])
+
+    def stop(self):
+        set_velocity(self.servos, [0, 0])
