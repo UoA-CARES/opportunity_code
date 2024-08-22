d6f46b0f6b02c685e7591755e81ef34cf0a9d43f
Afereti Pama <79831813+retinfai@users.noreply.github.com>
2024-08-23 10:04:48 +1200

    Dev/xbox controller (#5)
    Co-authored-by: AlvisOu <oualvis@gmail.com>

--- control/servo_test.py
+++ control/servo_test.py
@@ -1,19 +1,26 @@
 from cares_lib.dynamixel.Servo import Servo
-#from cares_lib.dynamixel.Servo import addresses
+from cares_lib.dynamixel.Servo import addresses
 import dynamixel_sdk as dxl
-from util import set_velocity, set_position
+from util import (
+    set_velocity, 
+    set_position,
+    _forward_velocity,
+    _backward_velocity,
+    _left_velocity,
+    _right_velocity)
 import keyboard
 import time
+from xbox_controller import XboxController
 
 def main():
     #Parameters
-    port = "COM3"
+    port = "/dev/ttyUSB0"
     baudrate = 1000000
     max = 4095
     min = 0
 
     models = input("Enter models for Servos (e.g. MX-106 XL-320): ").split(" ")
-    servo_ids = [1, 2, 3, 4]
+    servo_ids = [1, 2]
 
     protocols = [2] * len(models)
     for i in range(len(models)):
@@ -33,32 +40,56 @@ def main():
     # Velocity Input Range is 0->500 for CCW and 1024->1524 for CW
     standard_velocity = input("Set a standard velocity for Servos (e.g. 200): ")
     standard_velocity = int(standard_velocity)
+    joy = XboxController()
+
+    e_stop = True
 
     while True:
-        velocities = [0] * len(models)
+        velocities = [0] * len(servo_ids)
 
-        #To drive
-        if keyboard.is_pressed('w'):
-            velocities[0] = -standard_velocity
-            velocities[1] = standard_velocity
-        if keyboard.is_pressed('s'):
-            velocities[0] = standard_velocity
-            velocities[1] = -standard_velocity
-        if keyboard.is_pressed('a'):
-            velocities[0] = -standard_velocity
-            velocities[1] = -standard_velocity
-        if keyboard.is_pressed('d'):
-            velocities[0] = standard_velocity
-            velocities[1] = standard_velocity
+        control_inputs = joy.read()
+
+        left_joy_x, left_joy_y, right_trigger, left_trigger, A_button = control_inputs
+        left_joy_x = round(left_joy_x, 1)
+
+        # if not abs(left_joy_x) < 0.1 and abs(left_joy_y) < 0.1:
+
+        #     if abs(left_joy_y) > abs(left_joy_x):
+        #         velocities[0] = standard_velocity * right_trigger * left_joy_y / abs(left_joy_y)
+        #         velocities[1] = standard_velocity * right_trigger * left_joy_y / abs(left_joy_y)
+        #     else:
+        #         velocities[0] = standard_velocity * right_trigger * left_joy_x / abs(left_joy_x)
+        #         velocities[1] = standard_velocity * right_trigger * left_joy_x / abs(left_joy_x)
+    
+        # #To drive
+        if A_button > 0:
+            if e_stop == True:
+                e_stop = False
+            else:
+                e_stop = True
+            velocities = [0,0]
         
-        #To rotate mast
-        if keyboard.is_pressed('q'):
-            velocities[1] = standard_velocity
-        if keyboard.is_pressed('e'):
-            velocities[1] = -standard_velocity
-
+        if e_stop == False:
+            if right_trigger > 0.1:
+                val = round(500 * right_trigger/1)
+                velocities = _forward_velocity(val)
+            if left_trigger > 0.1:
+                val = round(500 * left_trigger/1)
+                velocities = _backward_velocity(val)
+            if left_joy_x > 0.5:
+                velocities = _left_velocity(standard_velocity)
+            if left_joy_x < -0.5: 
+                velocities = _right_velocity(standard_velocity)
+        else:
+            velocities = [0,0]
+        # #To rotate mast
+        # if keyboard.is_pressed('q'):
+        #     velocities[1] = standard_velocity
+        # if keyboard.is_pressed('e'):
+        #     velocities[1] = -standard_velocity
+        # print(velocities)
         set_velocity(list_of_servos, velocities)
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 def init_servo(port, baudrate, max, min, servo_init_info) -> dict[int, Servo]:
     servos = {}
