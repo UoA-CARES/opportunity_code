7b2521de3d15a7ddead5ec73df1fbf6569a09ae6
retinfai <aferetipama@gmail.com>
2024-08-24 19:06:19 +1200

    Add operating mode handles in main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     max_angular_velocity = 200
     max_linear_velocity = 500
 
-    # # Instantiate the Components
+    # Instantiate the Components
     # wheels = Wheels(
     #     max_linear_velocity=max_linear_velocity,
     #     max_angular_velocity=max_angular_velocity,
@@ -26,9 +26,23 @@ def main():
         if new_operating_mode and operating_mode != new_operating_mode:
             operating_mode = new_operating_mode
             sounds_effects.play_change_mode()
-            print('Changed')
         
-
+        if operating_mode == OperatingMode.DRIVE:
+            # Use Controller to drive the rover and control the mast
+            print("DRIVE")
+            # wheels.handle_inputs(control_inputs["wheels"])
+        elif operating_mode == OperatingMode.ROBOTIC_ARM:
+            # Use Controller to control the robotic arm
+            print("ROBOTIC_ARM")
+        elif operating_mode == OperatingMode.STATIONARY:
+            # Stationary mode
+            # Random robotic arm movement
+            # Camera track face, alien thing
+            print("STATIONARY")
+        elif operating_mode == OperatingMode.EMERGENCY_STOP:
+            # Send stop commands to all components
+            print("EMERGENCY_STOP")
+            # wheels.stop()
 
         time.sleep(0.01)
 
