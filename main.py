13d9e6eab234db1c3cd2c7ed87e236f5d116e09a
retinfai <aferetipama@gmail.com>
2024-08-27 14:37:10 +1200

    Add multithreading support for stationary mode

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import time
-from control import XboxController, OperatingMode, handle_operating_mode, Wheels, SoundEffects
+from control import (
+    XboxController,
+    OperatingMode,
+    handle_operating_mode,
+    Wheels,
+    SoundEffects,
+)
 import vlc
+import threading
+from threading import Event
+
 
 def main():
 
     max_angular_velocity = 200
     max_linear_velocity = 500
 
-    # Instantiate the Components
+    # # Instantiate the Components
     # wheels = Wheels(
     #     max_linear_velocity=max_linear_velocity,
     #     max_angular_velocity=max_angular_velocity,
@@ -15,18 +24,35 @@ def main():
 
     joy = XboxController()
     sounds_effects = SoundEffects()
+
     operating_mode = OperatingMode.STATIONARY
+
+    end_event = Event()
+    reset_event = Event()
+
+    arm_stationary_mode_thread = threading.Thread(
+        target=robotic_arm_stationary_mode, args=(end_event, reset_event)
+    )
+
+    camera_tracking_stationary_mode_thread = threading.Thread(
+        target=camera_tracking_stationary_mode, args=(end_event, reset_event)
+    )
+
 
     while True:
 
         control_inputs = joy.read()
 
         new_operating_mode = handle_operating_mode(control_inputs["operating_mode"])
-        
+
         if new_operating_mode and operating_mode != new_operating_mode:
             operating_mode = new_operating_mode
             sounds_effects.play_change_mode()
-        
+
+        # Kill stationary mode threads if not stationary mode
+        if operating_mode != OperatingMode.STATIONARY:
+            end_event.set()
+            
         if operating_mode == OperatingMode.DRIVE:
             # Use Controller to drive the rover and control the mast
             print("DRIVE")
@@ -39,12 +65,45 @@ def main():
             # Random robotic arm movement
             # Camera track face, alien thing
             print("STATIONARY")
+            
+            if end_event.is_set():
+                end_event.clear()
+                reset_event.set()
+
+            if not arm_stationary_mode_thread.is_alive():
+                arm_stationary_mode_thread.start()
+
+            if not camera_tracking_stationary_mode_thread.is_alive():
+                camera_tracking_stationary_mode_thread.start()
+
         elif operating_mode == OperatingMode.EMERGENCY_STOP:
             # Send stop commands to all components
             print("EMERGENCY_STOP")
             # wheels.stop()
 
         time.sleep(0.01)
+
+# Pass in arm object once integrated
+def robotic_arm_stationary_mode(end_event: Event, reset_event: Event):
+
+    while True:
+
+        if end_event.is_set():
+            reset_event.wait()
+    
+        print("Robotic Arm Stationary Mode")
+        time.sleep(1)
+
+# Pass in camera and mast object once integrated
+def camera_tracking_stationary_mode(end_event: Event, reset_event: Event):
+
+    while not end_event.is_set():
+        print("Camera Tracking Stationary Mode")
+
+        if end_event.is_set():
+            reset_event.wait()
+
+        time.sleep(1)
 
 
 if __name__ == "__main__":
