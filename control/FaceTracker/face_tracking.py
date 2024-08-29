6200bbfe7cfb431a336a270be486e146585b6199
Masahiro Kobayashi <75647798+engineerm-jp@users.noreply.github.com>
2024-08-29 13:22:13 +1200

    Face detection alien replacement (#13)
    Co-authored-by: CChenLyu <chen.lyu_@outlook.com>
    Co-authored-by: Afereti Pama <79831813+retinfai@users.noreply.github.com>

---
+++ control/FaceTracker/face_tracking.py
@@ -0,0 +1,152 @@
+import pyrealsense2 as rs
+import cv2 as cv
+import numpy as np
+import os 
+import threading
+import time
+
+class FaceTracker():
+    
+    IM_SIZE_H = 480
+    IM_SIZE_W = 640
+    MOVE_THRESHOLD = 1/6
+
+    def __init__(self, replacement_mode='all'):
+
+        self.replacement_mode = replacement_mode
+
+        self.tilt_up = 0 # 1 if true, -1 if need to tilt down, 0 otherwise        
+        self.move_right = 0 # 1 if true, -1 if need to move left, 0 otherwise        
+        self.running = True
+
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.color,640,480,rs.format.bgr8,30)
+        self.pipeline.start(self.config)
+        
+        self.face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+        alien_image_path = os.path.join(os.getcwd(),'alien_pictures/dab1.png')
+        #alien_image_path = os.path.join(os.getcwd(),'alien_pictures/greensmileface.png')
+        #alien_image_path = os.path.join(os.getcwd(),'alien_pictures/purplealien.png')
+        #alien_image_path = os.path.join(os.getcwd(),'alien_pictures/spongebob.png')
+
+        self.alien_image = cv.imread(alien_image_path)
+
+        # create a thead for face detection and replacement
+        self._face_detect_and_replace_thread = threading.Thread(
+            target=self._face_detect_and_replace, args=()
+        )
+
+        self._face_detect_and_replace_thread.daemon = True
+
+        # start thread
+        self._face_detect_and_replace_thread.start()
+        
+
+    def detect_face(self,frame):
+        gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray,1.05,6) 
+        return faces
+
+
+    def replace_face(self,frame, faces, alien_image):
+        biggest_face = max(faces, key=lambda x: x[2]*x[3]) 
+
+        if self.replacement_mode == 'all':
+            for (x, y, w, h) in faces:
+                alien_resized = cv.resize(alien_image,(w,h))
+                frame[y:y+h,x:x+w] = alien_resized 
+        else:
+            x,y,w,h = biggest_face[0], biggest_face[1], biggest_face[2], biggest_face[3]
+            alien_resized = cv.resize(alien_image,(w,h))
+            frame[y:y+h,x:x+w] = alien_resized 
+
+        return frame, biggest_face
+
+
+    def move_calculate(self,frame_shape, biggest):
+
+        x,y,w,h = biggest[0],biggest[1],biggest[2],biggest[3]
+        cx, cy = x+w//2, y+h//2 #center of biggest face
+        fx, fy = frame_shape[1]//2, frame_shape[0]//2 #center of frame (480,640,3)
+
+        move_x = fx - cx # +ve if fx greater than cx (i.e., to the left)
+        move_y = fy - cy # +ve if fy greater than cy (i.e., in the upper region)
+        # check the location of the biggest face in the horizontal direction
+        if move_x < -(self.IM_SIZE_W * self.MOVE_THRESHOLD):
+            self.move_right = 1
+        elif move_x > self.IM_SIZE_W * self.MOVE_THRESHOLD:
+            self.move_right = -1
+        else: 
+            self.move_right = 0
+        
+        # check the location of the biggest face in the vertical direction
+        if move_y > self.IM_SIZE_H * self.MOVE_THRESHOLD:
+            self.tilt_up = 1
+        elif move_y < -(self.IM_SIZE_H * self.MOVE_THRESHOLD):
+            self.tilt_up = -1
+        else: 
+            self.tilt_up = 0
+
+        return move_x, move_y
+
+
+    def realtime_track(self):
+
+        frames = self.pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+
+        if not color_frame:
+            return None
+        
+        frame = np.asanyarray(color_frame.get_data())
+        faces = self.detect_face(frame)
+
+        if len(faces) > 0:
+            frame, biggest_face = self.replace_face(frame, faces, self.alien_image)
+            _, _ = self.move_calculate(frame.shape, biggest_face)
+            
+        return frame
+
+
+    def release(self):
+        self.pipeline.stop()
+        cv.destroyAllWindows()
+        self.running = False
+
+
+    def get_move_vertical(self): # +ve if need to move up, -ve down, 0 otherwise
+        return self.tilt_up
+
+
+    def get_move_horizontal(self): # +ve if need to move right, -ve left, 0 otherwise
+        return self.move_right
+
+
+    def is_facetracker(self):
+        return self.running
+
+
+    def _face_detect_and_replace(self):
+        try:
+            while True:
+                frame = self.realtime_track()
+
+                if frame is not None:
+                    cv.imshow('ALien',frame) 
+
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        finally:
+            self.release()
+                
+
+if __name__ == '__main__':
+    facetracker = FaceTracker(replacement_mode='all')
+    
+    # example of how to get the direction for the mast
+    while facetracker.is_facetracker(): 
+        print(f'move right: {facetracker.get_move_horizontal()}')
+        print(f'move up: {facetracker.get_move_vertical()}')
+        time.sleep(1)
---
+++ control/face_detection.py
@@ -0,0 +1,171 @@
+import pyrealsense2 as rs
+import cv2 as cv
+import numpy as np
+import os 
+
+
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color,640,480,rs.format.bgr8,30)
+pipeline.start(config)
+
+face_cascade = cv.CascadeClassifier('/home/sdua078/chen_project/Rover/opportunity_code/control/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+#alien_image = cv.imread('/home/sdua078/chen_project/Rover/opportunity_code/control/test1.png',flags=cv.IMREAD_UNCHANGED)
+alien_image = cv.imread('/home/sdua078/chen_project/Rover/opportunity_code/control/alien.png')
+#print('img',alien_image)
+
+def detect_face(frame):
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.05,6) 
+    return faces
+
+
+def replace_face(frame, face, alien_image):
+    #for (x,y,w,h) in faces:
+    x,y,w,h = face[0], face[1],face[2],face[3]
+    alien_resized = cv.resize(alien_image,(w,h))
+    frame[y:y+h,x:x+w] = alien_resized     return frame
+
+def center(frame_shape, biggest_face):
+    x,y,w,h = biggest_face[0],biggest_face[1],biggest_face[2],biggest_face[3]
+    cx, cy = x+w//2, y+h//2 #center of biggest face
+    fx, fy = frame_shape[1]//2, frame_shape[0]//2 #center of frame 
+
+    move_x = cx-fx
+    move_y = cy-fy
+    return move_x, move_y
+
+
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        if not color_frame:
+            continue
+        
+        frame = np.asanyarray(color_frame.get_data())
+        faces = detect_face(frame)
+        
+        if len(faces) > 0:
+            biggest_face = max(faces, key=lambda x: x[2]*x[3])             
+            #x,y,w,h = biggest_face
+            #cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+            frame = replace_face(frame,biggest_face,alien_image)
+            #print(frame.shape) #(480,640,3),h*w*c         
+        cv.imshow('ALien',frame) ##target been replaced, show whole frame ，
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+finally:
+    pipeline.stop()
+    cv.destroyAllWindows()
+        
+
+
+
+# class FaceTracker:
+#     def __init__(self, camera_index, Mast=None):
+
+#         # Initialize the camera and face tracking parameters
+#         self.capture = cv.VideoCapture(camera_index) # open video stream
+#         self.frame_width = int(self.capture.get(cv.CAP_PROP_FRAME_WIDTH))
+#         self.frame_height = int(self.capture.get(cv.CAP_PROP_FRAME_HEIGHT))
+#         self.center_x = self.frame_width // 2
+#         self.center_y = self.frame_height // 2
+#         self.tolerance = 30  # How close to the center the object needs to be
+#         self.max_speed = 100  # Maximum speed for movement
+#         self.min_speed = 10   # Minimum speed for movement
+#         #self.mast = mast # The Mast object to be controlled
+
+
+#     def _get_face_position(self, frame):
+#         """
+#         Method for face detection.
+#         This method returns the x, y coordinates of the face's center.
+#         """
+#         # currently uses haar cascade, which is quite sensitive
+#         haar_cascade = cv.CascadeClassifier('haar_face.xml')          
+
+#         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # converts frame to grey scale
+
+#         faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3) # higher minNeighbors, the less sensitive
+
+#         # (x,y,w,h) = faces_rect # currently, only tracks the position of the first face of the array of detected faces
+#         idx = 0
+#         for i in range(len(faces_rect)):
+#             x,y,w,h = faces_rect[i]
+#             if i == 0: 
+#                 largest = w*h
+#             else: 
+#                 if largest < w*h: 
+#                     largest = w*h
+#                     idx = i
+
+#         x, y, w, h = faces_rect[0]
+#         cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+#         cv.imshow('Frame', frame) 
+
+#         return x, y
+
+
+#     def _calculate_speed(self, distance):
+#         """
+#         This method returns the speed based on the distance from the center. Faster when further away.
+#         """
+#         # If the distance is within tolerance, return 0 (no movement)
+#         if abs(distance) <= self.tolerance:
+#             return 0
+
+#         # Normalize the speed based on the distance, between min_speed and max_speed
+#         speed = int((abs(distance) - self.tolerance) / (max(self.center_x, self.center_y) - self.tolerance) * (self.max_speed - self.min_speed) + self.min_speed)
+
+#         return min(max(speed, self.min_speed), self.max_speed)
+
+
+#     def track_face(self):
+#         while True:
+#             isTrue, frame = self.capture.read()
+
+#             if not isTrue:
+#                 print("Failed to read frame")    
+#                 quit()
+
+#             # Get the object's current position
+#             face_x, face_y = self._get_face_position(frame)
+
+#             # Calculate the distance from the center
+#             x_distance = face_x - self.center_x
+#             y_distance = face_y - self.center_y
+
+#             # Calculate the speeds based on the distance
+#             x_speed = self._calculate_speed(x_distance)
+#             y_speed = self._calculate_speed(y_distance)
+
+#             # Adjust the camera position based o
+#         # If the distance is within tolerance, return 0 (no movement)
+#         if abs(distancn the distance and speed
+#             if x_speed > 0:
+#                 if x_distance < 0:
+#                     print("rotate counterclockwise")
+#                     # self.mast.rotate_counterclockwise(x_speed)
+#                 else:
+#                     print("rotate clockwise")
+#                     # self.mast.rotate_clockwise(x_speed)
+
+#             if y_speed > 0:
+#                 if y_distance < 0:
+#                     print("tilt up")
+#                     # self.mast.tilt_up(y_speed)
+#                 else:
+#                     print("tilt down")
+#                     # self.mast.tilt_down(y_speed)
+
+#             # cv.imshow('Frame', frame)
+
+#             if cv.waitKey(1) & 0xFF == ord('q'): # This would need to be integrated with the xbox controller
+#                 break
+
+#         # Release the camera and close any open windows
+#         self.capture.release()
+#         cv.destroyAllWindows()
