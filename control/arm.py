0b28aa49d52cad2e8191e478a2df259b7a6720bf
retinfai <aferetipama@gmail.com>
2024-09-12 16:25:36 +1200

    Clean up arm

--- control/arm.py
+++ control/arm.py
@@ -18,12 +18,9 @@ class Arm:
 
         self.servos = []
 
-        # ids = [5, 6, 7, 8]
         self.ids = [5, 7, 8]
-        # models =["MX-106", "MX-64", "MX-64", "XL430-W250-T"]
-        # models =["MX-28", "MX-106", "MX-64", "XL430-W250-T"]
+
         self.models =["MX-28Protocol2", "MX-64Protocol2", "XL430-W250-T"]
-        #Going toward the end of the arm, servos have ids 5-8
         
         self.current_pose = 0 
 
@@ -43,7 +40,6 @@ class Arm:
                 servo_factory.create_servo(
                     model=self.models[i],
                     port="/dev/ttyArm",
-                    # protocol=(1 if self.models[i][:2] in ["MX", "AX"] else 2),
                     protocol=(1 if self.models[i][:2] in ["AX"] else 2),
                     baudrate=1000000,
                     max=4095, #need to figure out range of motion, for now 45 degrees
@@ -121,7 +117,6 @@ class Arm:
     
     
     def handle_input(self, d_pad_x, d_pad_y, right_trigger, left_trigger):
-        # print([left_d_pad, right_d_pad, up_d_pad, down_d_pad, right_trigger, left_trigger])
 
         if d_pad_x == -1: # move vertical axis joint (arm base joint)
             self.move_joint_simple(joint_id=0, step=25, direc=-1, t=2000)
@@ -129,10 +124,10 @@ class Arm:
         elif d_pad_x == 1:# move vertical axis joint (arm base joint)
             self.move_joint_simple(joint_id=0, step=25, direc=1, t=2000)
 
-        elif d_pad_y == -1: # move arm joint (arm joint)
+        elif d_pad_y == 1: # move arm joint (arm joint)
             self.move_joint_simple(joint_id=1, step=5, direc=1, t=1500)
 
-        if d_pad_y == 1: # move arm joint (arm joint)
+        if d_pad_y == -1: # move arm joint (arm joint)
             self.move_joint_simple(joint_id=1, step=5, direc=-1, t=1500)
 
         elif right_trigger > 0.1: # move camera joint
@@ -141,138 +136,8 @@ class Arm:
         elif left_trigger > 0.1:
             self.move_joint_simple(joint_id=2, step=15, direc=-1, t=1000)
 
-        # else:
-        #     pass
-
-    def random_movement(self, step=5, t=3000):
-        
-
-        limit_0 = self.move_joint_simple(joint_id=0, step=step, direc=direc_0, t=t)
-        limit_1 = self.move_joint_simple(joint_id=1, step=step, direc=direc_1, t=t)
-        limit_2 = self.move_joint_simple(joint_id=2, step=step, direc=direc_2, t=t)
-
-        # Toggle direction
-        if limit_0 !=  0:
-            direc_0 *= -1
-        if limit_1 !=  0:
-            direc_1 *= -1
-        if limit_2 != 0:
-            direc_2 *= -1
-
-            # time.sleep(t//1000)
     def move_to_home(self):
         set_position(self.servos, self.home_position)
-
-
-    """
-    def move_arm_joints(self, joint_id: int=0, joint_angle: int=0, time: int=2000, camera_horiz=False):
-        
-        self.set_profile_time(joints=[0, 1, 2], time=time)
-
-        if joint_id == 0: 
-            gear_ratio = 32 / 24
-            joint_angle = (-joint_angle + 180) # 0 degrees when the servos are at 180 deg
-            joint_angle = int((joint_angle) * gear_ratio * 4095 / 360) # add the effect of the gear ratio
-            set_position(self.servos[0], [joint_angle])
-        
-        # if self.servos.model[joint_id] == 'MX-64': 
-        elif joint_id == 1: 
-            
-            joint_angle_offset = (-joint_angle + 120) # 0 degrees when the servos are at 180 deg
-
-            ARM_MIN = 120 - (-20)             
-            ARM_MAX = 120 - (-45) # 165 # 120 - 165 = - 45 deg
-
-            if joint_angle_offset < ARM_MIN: 
-                joint_angle_offset = ARM_MIN
-            elif joint_angle_offset > ARM_MAX: 
-                joint_angle_offset = ARM_MAX
-            else: 
-                pass
-            
-            self.set_profile_time([joint_id], time)
-            joint_angle_offset  = int((joint_angle_offset) * 4095 / 360) # add the effect of the gear ratio
-            set_position([self.servos[1]], [joint_angle_offset])
-            
-            # keep the camera flat
-            if camera_horiz == True:
-                joint_angle_camera = -(joint_angle) + 180
-                joint_angle_camera = int((joint_angle_camera) * 4095 / 360) # no offset
-                set_position([self.servos[2]], [joint_angle_camera])
-        
-        elif joint_id == 2: 
-            servo_1_pos = get_servo_position(self.servos[1])
-            joint_angle_camera = -(joint_angle) + 180
-            joint_angle_camera = int((joint_angle_camera) * 4095 / 360) # no offset
-            set_position([self.servos[2]], [joint_angle_camera])
-
-        else: 
-            return
-    """
-
-    '''
-    # simplified inverse kinematics for 3 DOF arm (rough approximation of the real arm)
-    def simplified_inverse_kinematics(self, position: list[int]) -> list[int]:
-
-        # define arm parameteres
-        
-        # L2, L3 = 1, 1 # TODO: update this
-        x, y, z = position
-
-        A = np.sqrt(x**2 + y**2)
-        B = np.sqrt(A**2 + z**2)
-        alpha = np.arctan2(z, A)     
-        beta = self._cosines_law(self.L2, B, self.L3)
-        gamma = self._cosines_law(self.L2, self.L3, B)
-        
-        # calculate theta1, theta2, theta3
-        theta1 = np.arctan2(y, x)
-        theta2 = alpha + beta
-        theta3 = -(np.pi - gamma)
-        
-        # +ve using right hand rule
-        # theta1: z-axis (pointing up), 0 degrees is along x-axis (pointing forward)
-        # theta2: y-axis (pointing left), 0 degrees is along x-axis (pointing forward)
-        # theta3: y-axis (pointing left), 0 degrees is along x-axis (pointing forward)
-        return [theta1, theta2, theta3] 
-    '''
-
-    '''
-    def move_arm(self, position: list[int]):
-        
-        gear_ratio = 32 / 24
-        position = np.array(position)
-        
-        position[0] = position[0] + self.L2 + self.L3 - 100 # make the position relative to the most extended state -10 cm
-        theta = self.simplified_inverse_kinematics(position) 
-        theta = np.array(theta) * 180 / np.pi
-
-        theta = (-theta + 180) # 0 degrees when the servos are at 180 deg
-        theta[0] = int((theta[0]*gear_ratio) * 4095 / 360) # add the effect of the gear ratio
-        theta[1] = int((theta[1] - 30) * 4095 / 360) # add 30 degrees offset 
-        theta[2] = int((theta[2]) * 4095 / 360) # no offset
-        
-        # move servo
-        set_position(self.servos, [int(theta[0]), int(theta[1]), int(theta[2])])
-
-        '''
-    
-    '''
-    @staticmethod
-    def _cosines_law(l1, l2, opposite_l):
-        return np.arccos((l1**2 + l2**2 - opposite_l**2)/(2*l1*l2))
-    '''
-
-
-
-
-# arm = Arm()
-# arm.set_profile_time(joint=1, time=4000)
-# arm.move_arm_joints(joint_id=1, joint_angle=-25, time=4000)
-# arm.move_joint_simple(joint_id=1, step=5, direc=1, time=3000)
-# arm.move_arm_joints(joint_id=1, joint_angle=0, time=2000)
-
-# arm.move_arm_joints(joint_id=2, joint_angle=0, time=2000)
 
 
 
